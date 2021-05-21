040b88607dd0c0605e0a0922c12672a5bf09b526
warpreaktor <warpreaktor@gmail.com>
2021-05-21 13:51:22 +0300

    go

---
+++ desktop/src/com/gb/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.gb.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.gb.StarGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new StarGame(), config);
+	}
+}
